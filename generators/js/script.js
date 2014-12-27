9504f241e21d85ffcfab3f77659447a9e9218831
Jacob Mortensen <jmo@trustpilot.com>
2014-12-27 06:01:08 +0100

    Using Trustpilot api with csp.

--- generators/js/script.js
+++ generators/js/script.js
@@ -4,20 +4,32 @@ var csp = require("js-csp");
 
 var f = a => console.dir(a);
 
-f("abe");
+function *dingo(){
+  yield 7;
+  yield 8;
+  yield 9;
+}
 
 function *foo(x) {
     var y = yield 123;
     yield (2 + y);
+    yield* dingo();
     yield 3 + x;
 }
 
-var it = foo(33);
+var logGenerator = g => {
+  for(var x of g){
+    console.log("Value " + x);
+  }
+}
 
+logGenerator(foo(1));
+
+var it = foo(23);
 f(it.next().value);
-f(it.next(3));
-f(it.next());
-f(it.next());
+f(it.next(3).value);
+f(it.next().value);
+f(it.next().value);
 
 var listen = function(id, eventName) {
   var element = document.getElementById(id);
@@ -39,6 +51,42 @@ window.onload = () => {
     for(;;){
       var event = yield csp.take(buttonChan);
       f(event);
+    }
+  });
+
+
+  var searchChan = listen("search-button", "click");
+
+  csp.go(function*(){
+    var searchInput = document.getElementById("search-input");
+
+    for(;;){
+      var event = yield csp.take(searchChan);
+      var data = yield csp.take(businessSearch(searchInput.value));
+
+      var requests = [];
+      for(var bu of data.businessUnits){
+        requests.push(businessData(bu.id));
+      }
+
+      // simple presentation
+      var root = document.getElementById("search-results");
+      root.innerHTML = '';
+      var numFound = document.createElement('div');
+      numFound.appendChild(document.createTextNode("Found for " + searchInput.value + " returned " + data.businessUnits.length));
+      root.appendChild(numFound);
+
+      for(var r of requests){
+        var bu = yield r;
+        var div = document.createElement('div');
+        var elm = document.createTextNode("Name: " + bu.displayName + ", " + bu.trustScore);
+        div.appendChild(elm);
+        root.appendChild(div);
+      }
+
+      searchInput.value = '';
+      searchInput.focus();
+      f(data);
     }
   });
 
@@ -75,3 +123,42 @@ window.onload = () => {
     }
   });
 };
+
+
+function httpRequest(url) {
+  var ch = csp.chan();
+  var req = new XMLHttpRequest();
+  req.onload = function() {
+    if(req.status === 200) {
+      csp.putAsync(ch, this.responseText);
+    }
+    else {
+      csp.putAsync(ch, new Error(this.responseText));
+    }
+  }
+  req.open('get', url, true);
+  req.send();
+  return ch;
+}
+
+function jsonRequest(url) {
+  return csp.go(function*() {
+    var value = yield csp.take(httpRequest(url));
+    if(!(value instanceof Error)) {
+      value = JSON.parse(value);
+    }
+    return value;
+  });
+}
+
+
+var apiKey = '&apikey=IZIKhLfUQejkeYPBBWO8GH7eQPdtdNQX';
+var baseUrl = 'https://api.trustpilot.com/v1/business-units/';
+
+var businessSearch = function(name){
+  return jsonRequest(baseUrl + 'search?query=' + name + '?' + apiKey);
+}
+
+var businessData = function(id){
+  return jsonRequest(baseUrl + id + '?' + apiKey);
+}
